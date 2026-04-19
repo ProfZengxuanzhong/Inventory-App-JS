90d2dce987e1f4c945adad1def923a97c8137187
ProfZengxuanzhong <3106336120lsc@gmail.com>
2026-04-19 16:37:34 +0800

    Update storage.test.js

--- src/js/storage.test.js
+++ src/js/storage.test.js
@@ -1,16 +1,114 @@
-describe('Storage 模块测试', () => {
+import Storage from './storage.js';
+
+describe('Storage 类测试', () => {
   beforeEach(() => {
     localStorage.clear();
   });
 
-  test('localStorage 应该可以正常读写', () => {
-    localStorage.setItem('testKey', 'testValue');
-    expect(localStorage.getItem('testKey')).toBe('testValue');
+  describe('getProducts', () => {
+    test('当 localStorage 为空时，应该返回空数组', () => {
+      const products = Storage.getProducts;
+      expect(products).toEqual([]);
+    });
+
+    test('当 localStorage 有数据时，应该返回解析后的产品数组', () => {
+      const mockProducts = [
+        { id: 1, name: '产品A', price: 100 },
+        { id: 2, name: '产品B', price: 200 }
+      ];
+      localStorage.setItem('products', JSON.stringify(mockProducts));
+      
+      const products = Storage.getProducts;
+      expect(products).toEqual(mockProducts);
+    });
   });
 
-  test('localStorage 删除功能正常', () => {
-    localStorage.setItem('testKey', 'testValue');
-    localStorage.removeItem('testKey');
-    expect(localStorage.getItem('testKey')).toBeNull();
+  describe('getCategories', () => {
+    test('当 localStorage 为空时，应该返回空数组', () => {
+      const categories = Storage.getCategories();
+      expect(categories).toEqual([]);
+    });
+
+    test('当 localStorage 有数据时，应该返回解析后的分类数组', () => {
+      const mockCategories = ['电子产品', '服装', '食品'];
+      localStorage.setItem('categories', JSON.stringify(mockCategories));
+      
+      const categories = Storage.getCategories();
+      expect(categories).toEqual(mockCategories);
+    });
+  });
+
+  describe('saveProducts', () => {
+    test('应该正确保存产品数组到 localStorage', () => {
+      const productsToSave = [
+        { id: 1, name: '产品A' },
+        { id: 2, name: '产品B' }
+      ];
+      
+      Storage.saveProducts(productsToSave);
+      
+      const savedProducts = JSON.parse(localStorage.getItem('products'));
+      expect(savedProducts).toEqual(productsToSave);
+    });
+
+    test('保存空数组应该正确存储', () => {
+      Storage.saveProducts([]);
+      
+      const savedProducts = JSON.parse(localStorage.getItem('products'));
+      expect(savedProducts).toEqual([]);
+    });
+  });
+
+  describe('saveCategories', () => {
+    test('应该正确保存分类数组到 localStorage', () => {
+      const categoriesToSave = ['分类1', '分类2', '分类3'];
+      
+      Storage.saveCategories(categoriesToSave);
+      
+      const savedCategories = JSON.parse(localStorage.getItem('categories'));
+      expect(savedCategories).toEqual(categoriesToSave);
+    });
+  });
+
+  describe('removeProduct', () => {
+    test('应该正确删除指定 ID 的产品', () => {
+      const initialProducts = [
+        { id: 1, name: '产品A' },
+        { id: 2, name: '产品B' },
+        { id: 3, name: '产品C' }
+      ];
+      Storage.saveProducts(initialProducts);
+      
+      Storage.removeProduct(2);
+      
+      const remainingProducts = Storage.getProducts;
+      expect(remainingProducts).toEqual([
+        { id: 1, name: '产品A' },
+        { id: 3, name: '产品C' }
+      ]);
+    });
+
+    test('删除不存在的 ID 时，数组应该保持不变', () => {
+      const initialProducts = [
+        { id: 1, name: '产品A' },
+        { id: 2, name: '产品B' }
+      ];
+      Storage.saveProducts(initialProducts);
+      
+      Storage.removeProduct(999);
+      
+      const remainingProducts = Storage.getProducts;
+      expect(remainingProducts).toEqual(initialProducts);
+    });
+
+    test('当只有一个产品时删除它，应该返回空数组', () => {
+      const initialProducts = [{ id: 1, name: '唯一产品' }];
+      Storage.saveProducts(initialProducts);
+      
+      Storage.removeProduct(1);
+      
+      const remainingProducts = Storage.getProducts;
+      expect(remainingProducts).toEqual([]);
+    });
   });
 });
